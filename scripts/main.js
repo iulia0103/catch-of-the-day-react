aebb7096dcddab0ac1722e538cea7ca67ac1bfea
iulia0103 <iulia.leancu@gmail.com>
2016-06-22 16:49:36 +0200

    main components for the application layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,58 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+/*Applictation*/
+var App = React.createClass({
+
+  render: function(){
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+
+        <Order />
+
+        <Inventory />
+      </div>
+    )
+  }
+
+});
+
+/*Header*/
+var Header = React.createClass({
+
+  render: function(){
+    return(
+      <p>Header</p>
+    )
+  }
+
+});
+
+/*Order*/
+var Order = React.createClass({
+
+  render: function(){
+    return(
+      <p>Order</p>
+    )
+  }
+
+});
+
+/*Inventory*/
+var Inventory = React.createClass({
+
+  render: function(){
+    return(
+      <p>Inventory</p>
+    )
+  }
+
+});
+
 /*Store Picker
 This will let us make <StorePicker/>*/
 
@@ -19,4 +71,4 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
